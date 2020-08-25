a9e30998286976ac4f37fe008ad1acc938650de2
Steffen Bank <Steffen.Bank@polhus.rootdom.dk>
2020-08-25 15:42:36 +0200

    added goal per minute function

--- R/sco_player_contribution_matrix.R
+++ R/sco_player_contribution_matrix.R
@@ -49,32 +49,32 @@ sco_player_contribution_matrix <- function(year_input, league_input) {
     ggplot2::geom_vline(xintercept = 0.15, color = "grey20", alpha = 0.4) +
   ggrepel::geom_text_repel(data = contribution %>%
                     dplyr::filter(goal_contrib > 0.25 | assist_contrib > 0.15),
-                                  aes(label = Player, family = "Roboto Condensed", fontface = "bold"),
+                                  ggplot2::aes(label = Player, family = "Roboto Condensed", fontface = "bold"),
                   seed = 15, size = 5,
                   min.segment.length = 0, segment.color = "red",
                   point.padding = 0.5) +
-    ggplot2::scale_x_continuous(labels = percent_format(accuracy = 1),
+    ggplot2::scale_x_continuous(labels = scales::percent_format(accuracy = 1),
                      breaks = c(0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3),
                      limits = c(0, 0.3)) +
-    ggplot2::scale_y_continuous(labels = percent_format(accuracy = 1),
+    ggplot2::scale_y_continuous(labels = scales::percent_format(accuracy = 1),
                      breaks = c(0, 0.1, 0.2, 0.3, 0.4, 0.5),
                      limits = c(0, 0.5)) +
-    ggplot2::labs(title = glue("Goal Contribution Matrix: ", title_input),
+    ggplot2::labs(title = paste0("Goal Contribution Matrix: ", title_input),
        subtitle = "Team goal involvement as percentage of total club goals* and/or assists.",
-       caption = glue("
+       caption = paste0("
                       *Own goals not counted in total/player can play at multiple clubs
                       Data: soccerway.com
                       By: @SteffenBank"),
        x = "Percentage of club goals assisted",
        y = "Percentage of club goals scored") +
   ggplot2::theme_minimal() +
-  ggplot2::theme(text = element_text(family = "Roboto Condensed"),
-        title = element_text(size = 18),
-        plot.subtitle = element_text(size = 16),
-        plot.caption = element_text(size = 10),
-        axis.title = element_text(size = 14),
-        axis.text = element_text(size = 12),
-        panel.grid.minor.x = element_blank()) -> goal_contribution_matrix
+  ggplot2::theme(text = ggplot2::element_text(family = "Roboto Condensed"),
+        title = ggplot2::element_text(size = 18),
+        plot.subtitle = ggplot2::element_text(size = 16),
+        plot.caption = ggplot2::element_text(size = 10),
+        axis.title = ggplot2::element_text(size = 14),
+        axis.text = ggplot2::element_text(size = 12),
+        panel.grid.minor.x = ggplot2::element_blank()) -> goal_contribution_matrix
 
 goal_contribution_matrix
 
